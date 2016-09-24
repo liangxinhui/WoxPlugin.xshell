06bea6e038a69333d832d4a0a7582efc3426f2fc
liangxh <liangxinhui@qq.com>
2016-09-24 17:09:42 +0800

    init

---
+++ Config.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Wox.Plugin.XShell
+{
+    
+
+    class Config
+    {
+        public string XShellExePath { get; set; }
+        public string XShellSessionsFolder { get; set; }
+        [JsonIgnore]
+        public string  m_path;
+
+
+        public void Save()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            using (StreamWriter sw = new StreamWriter(m_path))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, this);
+            }
+        }
+
+        public static  Config GetDefaultConfig(string strPath)
+        {
+            Config cfg = new Config();
+            cfg.m_path = strPath;
+            cfg.XShellExePath = @"C:\Program Files (x86)\NetSarang\Xshell 5\Xshell.exe";
+            cfg.XShellSessionsFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\NetSarang\\Xshell\\Sessions";
+            return cfg;
+        }
+
+        public static bool Load(string filePath, ref Config cfg, ref string strError)
+        {
+            if (!File.Exists(filePath))
+            {
+                cfg = GetDefaultConfig(filePath);
+                cfg.Save();
+                return true;
+            }
+
+            try
+            {
+                string str = File.ReadAllText(filePath);
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+                settings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                cfg = JsonConvert.DeserializeObject<Config>(str, settings);
+            }
+            catch(System.Exception ex)
+            {
+                cfg = GetDefaultConfig(filePath);
+                cfg.Save();
+            }finally
+            {
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Plugin;
+
+namespace Wox.Plugin.XShell 
+
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext _context;
+        private string _PATH_XSHELL_EXE;
+        private string _PATH_XSHELL_SESSIONS_FOLDER;
+        private bool m_bValidPath = false;
+        public void Init(PluginInitContext context)
+        {
+            _context = context;
+
+            Config cfg = new Config();
+            string strErr = "";
+            if(Config.Load(_context.CurrentPluginMetadata.PluginDirectory + "\\config.json", ref cfg, ref strErr))
+            {
+                _PATH_XSHELL_EXE = cfg.XShellExePath;
+                _PATH_XSHELL_SESSIONS_FOLDER = cfg.XShellSessionsFolder;
+                if(File.Exists(_PATH_XSHELL_EXE) && Directory.Exists(_PATH_XSHELL_SESSIONS_FOLDER))
+                {
+                    m_bValidPath = true;
+                }
+            }
+        }
+
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+
+            if (!m_bValidPath)
+            {
+                results.Add(new Result()
+                {
+                    Title = "xsh",
+                    SubTitle = "配置不正确，请安装xshell并正确配置config.json",
+                    IcoPath = "app.png"
+                });
+                return results;
+            }
+
+
+            string[] strFilters = query.SecondToEndSearch.Split();
+            string[] strXshList = GetXshList();
+            string[] strFilteredXshList = FilterStringList(strXshList, strFilters);
+            foreach(string xsh in strFilteredXshList)
+            {
+                results.Add(new Result()
+                {
+                    Title = xsh,
+                    //SubTitle = "xshell",
+                    IcoPath = "app.png",  //相对于插件目录的相对路径
+                    Action = e =>
+                    {
+                        // 处理用户选择之后的操作
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo
+                            {
+                                FileName = _PATH_XSHELL_EXE,
+                                UseShellExecute = true,
+                                Arguments = _PATH_XSHELL_SESSIONS_FOLDER + "\\" + xsh + ".xsh"
+                            });
+                        }
+                        catch (Win32Exception)
+                        {
+                            var name = $"Plugin: {_context.CurrentPluginMetadata.Name}";
+                            var message = "Can't open this file";
+                            _context.API.ShowMsg(name, message, string.Empty);
+                        }
+                        _context.API.ChangeQuery("");
+                        //返回false告诉Wox不要隐藏查询窗体，返回true则会自动隐藏Wox查询窗口
+                        return true;
+                    }
+                });
+            }
+            return results;
+        }
+
+
+        private string[] FilterStringList(string[] strList, string[] filterList)
+        {
+            List<string> retList = new List<string>();
+            foreach(string str in strList)
+            {
+                bool bAllMatch = true;
+                foreach(string fil in filterList)
+                {
+                    if(str.IndexOf(fil) < 0)
+                    {
+                        bAllMatch = false;
+                        break;
+                    }
+                }
+                if(bAllMatch)
+                {
+                    retList.Add(str);
+                }
+            }
+            return retList.ToArray();
+        }
+
+        private string[] GetXshList()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_PATH_XSHELL_SESSIONS_FOLDER);
+            var fileInfoList = dirInfo.GetFiles("*.xsh");
+            List<string> strList = new List<string>();
+            foreach(var fileInfo in fileInfoList)
+            {
+                strList.Add(Path.GetFileNameWithoutExtension(fileInfo.Name));
+            }
+            return strList.ToArray();
+        }
+
+    }
+}
